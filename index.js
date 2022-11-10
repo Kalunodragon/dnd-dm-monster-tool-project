4e7b161f4db3ec7c6a690e5c062294f2527f2942
Andrew <andrewonulak@gmail.com>
2022-11-10 15:20:14 -0700

    got fetch to get each individual monster data, also added a simple display to DOM

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 // Variables
 const formInput = document.getElementById('input-text-feild')
 const formSubmit = document.getElementById('search-button')
-
+const monsterCards = document.getElementById('monster-cards')
 // Event Listeners
-formSubmit.addEventListener('click', displayMonsters)
+formSubmit.addEventListener('click', initialMonstersFetch)
 
 // Functions
-function displayMonsters(e){
+function fetchMonsters(e){
     e.preventDefault()
-    getMonstersFetch()
+    initialMonstersFetch()
+}
+
+function displayMonsters(monster){
+    const h3 = document.createElement('h3')
+    h3.innerText = monster.name
+
+    const p = document.createElement('p')
+    p.innerText = monster.alignment
+
+    const h4 = document.createElement('h4')
+    h4.innerText = monster.hit_points
+
+    const div = document.createElement('div')
+    div.id = `${monster.name}`
+    div.append(h3, h4, p)
+    monsterCards.appendChild(div)
 }
 
 
 // Fetch Functions
-function getMonstersFetch(){
+function initialMonstersFetch(){
     return fetch('https://www.dnd5eapi.co/api/monsters/')
     .then(resp => resp.json())
     .then(data => {
         console.log(data)
-        const monsterArray = data.id
-        console.log(monsterArray)
+        const monsterArray = data.results
+        monsterArray.forEach(monster => {
+            singleMonsterFetch(`${monster.url}`)
+        })
+    })
+}
+
+function singleMonsterFetch(URL){
+    return fetch(`https:www.dnd5eapi.co${URL}`)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+        displayMonsters(data)
     })
 }
