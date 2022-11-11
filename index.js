8342832dbc6ece7c3d1116412e22d21d7621d6f6
Andrew <andrewonulak@gmail.com>
2022-11-11 16:00:12 -0700

    changed the fetch to an instant fetch, search will search for monsters on the page

--- index.js
+++ index.js
@@ -3,27 +3,29 @@ const formInput = document.getElementById('input-text-feild')
 const formSubmit = document.getElementById('search-button')
 const monsterCards = document.getElementById('monster-cards')
 // Event Listeners
-formSubmit.addEventListener('click', initialMonstersFetch)
+// formSubmit.addEventListener('click', initialMonstersFetch)
+
+initialMonstersFetch()
 
 // Functions
-function fetchMonsters(e){
-    e.preventDefault()
-    initialMonstersFetch()
-}
+// function fetchMonsters(e){
+//     e.preventDefault()
+//     initialMonstersFetch()
+// }
 
 function displayMonsters(monster){
-    const h3 = document.createElement('h3')
-    h3.innerText = monster.name
+    const h2 = document.createElement('h2')
+    h2.innerText = monster.name
 
     const p = document.createElement('p')
-    p.innerText = monster.alignment
+    p.innerText = 'Alignment: ' + monster.alignment
 
     const h4 = document.createElement('h4')
-    h4.innerText = monster.hit_points
+    h4.innerText = 'Hit Points: ' + monster.hit_points
 
     const div = document.createElement('div')
     div.id = `${monster.name}`
-    div.append(h3, h4, p)
+    div.append(h2, h4, p)
     monsterCards.appendChild(div)
 }
 
